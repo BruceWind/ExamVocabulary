2f9d044391c74c6fcdd6370f24eb308723b4b569
Bruce <hide@hide.com>
2023-06-09 16:45:45 +0800

    [doc]: add comment.

--- excluding.py
+++ excluding.py
@@ -2,17 +2,19 @@
 word_list1 = set()
 word_list2 = set()
 
+# reading the first file that has less words.
 with open('CET.txt', 'r') as f1:
   for line in f1:
         word = line.split()[0].strip()
         word_list1.add(word)
 
-
+# reading the 2nd file that has more words than the first.
 with open('IELTS_8000.txt', 'r') as f2:
     for line in f2:
         word = line.split()[0].strip()
         word_list2.add(word)
 
+# to remove duplicated words. and then write into file excluded.txt.
 word_list2 = word_list2 - word_list1
 with open('excluded.txt', 'w') as f3:
     for word in word_list2:
