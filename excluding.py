ee0ca6b7a2bbca78bbd7f31c6dea9ffbf9ccd310
Bruce <hide@hide.com>
2023-06-10 10:22:07 +0800

    [feat]: update words.

--- excluding.py
+++ excluding.py
@@ -3,15 +3,16 @@ word_list1 = set()
 word_list2 = set()
 
 # reading the first file that has less words.
-with open('CET4.txt', 'r') as f1:
+with open('IELTS_4000.txt', 'r') as f1:
   for line in f1:
         word = line.split()[0].strip()
         word_list1.add(word)
 
 # reading the 2nd file that has more words than the first.
-with open('IELTS_excluded_TOEFL.txt', 'r') as f2:
+with open('IELTS_core_words_with_CN.txt', 'r') as f2:
     for line in f2:
         word = line.split()[0].strip()
+        word = word.replace("*", "") 
         word_list2.add(word)
 
 # to remove duplicated words. and then write into file excluded.txt.
